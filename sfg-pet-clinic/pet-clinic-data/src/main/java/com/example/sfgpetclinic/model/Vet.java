c39290bf1166be1af07aa2c1b6757d526a24cbbd
Łukasz Staniszewski <2277lukas@gmail.com>
2019-08-17 19:10:14 +0200

    Vets and specialties converted to JPA Entity

--- sfg-pet-clinic/pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Vet.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Vet.java
@@ -1,10 +1,17 @@
 package com.example.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.jar.JarEntry;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)//załaduję wszystkie zależności
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id")
+            ,inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialities = new HashSet<>();
 
     public Set<Specialty> getSpecialities() {
