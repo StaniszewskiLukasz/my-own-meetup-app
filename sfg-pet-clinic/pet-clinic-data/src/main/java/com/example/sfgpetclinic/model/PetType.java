c0c022fac250955feaca5f88952688f14fcd4ed8
Łukasz Staniszewski <2277lukas@gmail.com>
2019-08-20 21:31:12 +0200

    Refactoring for Lombok

--- sfg-pet-clinic/pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/PetType.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/PetType.java
@@ -1,19 +1,18 @@
 package com.example.sfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity {
 
     @Column(name = "name")
     private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- sfg-pet-clinic/pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Vet.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Vet.java
@@ -1,10 +1,17 @@
 package com.example.sfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.jar.JarEntry;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
@@ -13,12 +20,4 @@ public class Vet extends Person {
     @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id")
             ,inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialities = new HashSet<>();
-
-    public Set<Specialty> getSpecialities() {
-        return specialities;
-    }
-
-    public void setSpecialities(Set<Specialty> specialities) {
-        this.specialities = specialities;
-    }
 }
