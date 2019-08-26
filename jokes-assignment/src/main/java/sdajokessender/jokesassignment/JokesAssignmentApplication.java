bfa41e5edd5663bbe4ff6a402ff22a3aca17fee8
Łukasz Staniszewski <2277lukas@gmail.com>
2019-08-26 16:03:47 +0200

    zrobiony szkielet aplikacji

---
+++ jokes-assignment/src/main/java/sdajokessender/jokesassignment/JokesAssignmentApplication.java
@@ -0,0 +1,13 @@
+package sdajokessender.jokesassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokesAssignmentApplication.class, args);
+    }
+
+}
